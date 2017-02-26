ef8a3a453ff818a361cd49bbde2ef2ae1f49eaea
Chanuka <Akunach@DESKTOP-4UGQ5NR>
2017-02-26 04:11:51 +0000

    Initial commit

---
+++ ComicBookStore/ComicBookStore/Controllers/ComicBookStoreController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookStore.Controllers
+{
+    public class ComicBookStoreController : Controller
+    {
+        public string Detail()
+        {
+            return "Hellow";
+        }
+    }
+}
