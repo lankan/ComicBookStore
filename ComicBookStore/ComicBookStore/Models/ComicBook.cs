35a085c49ac83fca6437b6ce30ede46c4c4b1119
Chanuka <Akunach@DESKTOP-4UGQ5NR>
2017-02-26 11:52:17 +0000

    updated comic book class

--- ComicBookStore/ComicBookStore/Models/ComicBook.cs
+++ ComicBookStore/ComicBookStore/Models/ComicBook.cs
@@ -13,5 +13,23 @@ namespace ComicBookStore.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artist { get; set; }
         public bool favourite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + "#" + IssueNumber; 
+            }
+        }
+
+        //series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-".ToLower()) + "-" + IssueNumber + ".jpg";
+            }
+                
+        }
     }
 }
