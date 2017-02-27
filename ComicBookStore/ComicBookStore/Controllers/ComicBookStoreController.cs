f0701eb3af4de6d635396cf1f3393c72b26061fd
Chanuka <Akunach@DESKTOP-4UGQ5NR>
2017-02-27 22:09:29 +0000

    + controlller

--- ComicBookStore/ComicBookStore/Controllers/ComicBookStoreController.cs
+++ ComicBookStore/ComicBookStore/Controllers/ComicBookStoreController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookStore.Models;
+﻿using ComicBookStore.Data;
+using ComicBookStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,27 +10,26 @@ namespace ComicBookStore.Controllers
 {
     public class ComicBookStoreController : Controller
     {
-        public ActionResult Detail()
+        
+        private ComicBookRepository _comicBookRepository = null;
+
+
+
+        public ComicBookStoreController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+           
+
+        public ActionResult Detail(int? Id)
 
         {
-            var comicbook = new ComicBook()
+            if (Id ==null)
             {
-                private ComicBookRepository comicBookRepo = new ComicBookRepository();
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artist = new Artist[]
-                {
-                   new Artist{role ="Script" , name="Dan Slott"},
-                   new Artist{role ="Pencils" , name="Humberto Ramos"},
-                   new Artist{role ="Inks" , name="Victor Olazaba"},
-                   new Artist{role ="Colors" , name="Edgar Delgado"},
-                   new Artist{role ="Letters" , name="Chris Eliopoulos"},
-                }
-
-            };
-            
-            return View(comicbook);
+                HttpNotFound();
+            }
+            var returnComic = _comicBookRepository.getComicBook(Id.Value);
+            return View(returnComic);
 
         }
     }
